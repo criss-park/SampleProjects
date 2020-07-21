b3650fd2e540b59d509a00925e5ca7d7f78cba38
Jonghun Park <jonghun.criss.android@gmail.com>
2020-07-21 15:01:11 +0900

    add Kakao Login sample

---
+++ kakaoLogin/src/main/java/com/simplify/kakaoLogin/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.simplify.kakaoLogin.controller;
+
+import com.simplify.kakaoLogin.service.KakaoAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private KakaoAPI kakaoAPI;
+
+    @RequestMapping(value="/")
+    public String index() {
+
+        return "index";
+    }
+
+    @RequestMapping(value="/login")
+    public String login(@RequestParam("code") String code, HttpSession session) {
+        String access_Token = kakaoAPI.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+
+        //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
+
+        return "index";
+    }
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession session) {
+        kakaoAPI.kakaoLogout((String)session.getAttribute("access_Token"));
+        session.removeAttribute("access_Token");
+        session.removeAttribute("userId");
+        return "index";
+    }
+
+}
