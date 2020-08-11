ce2495eabc47c502549664a35dc784deaa80dcef
Jonghun Park <jonghun.criss.android@gmail.com>
2020-08-11 11:32:34 +0900

    http request demo

--- httpReq/src/main/java/com/simplify/httpReq/controller/RequestController.java
+++ httpReq/src/main/java/com/simplify/httpReq/controller/RequestController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Iterator;
-
 @Controller
 public class RequestController {
 
@@ -18,30 +16,12 @@ public class RequestController {
     @RequestMapping(value="/req/form")
     public @ResponseBody String reqForm(){
         JsonNode node = requestService.requestHttpForm();
-
-//        if(node != null){
-//            Iterator<String> itr = node.fieldNames();
-//            while(itr.hasNext()){
-//                String key = itr.next();
-//                System.out.println(key + " : " + node.get(key));
-//            }
-//        }
-
         return "/req/form";
     }
 
     @RequestMapping(value="/req/json")
     public @ResponseBody String reqJson(){
         JsonNode node = requestService.requestHttpJson();
-
-//        if(node != null){
-//            Iterator<String> itr = node.fieldNames();
-//            while(itr.hasNext()){
-//                String key = itr.next();
-//                System.out.println(key + " : " + node.get(key));
-//            }
-//        }
-
         return "/req/json";
     }
 }
