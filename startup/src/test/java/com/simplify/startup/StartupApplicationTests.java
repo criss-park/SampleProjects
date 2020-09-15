543cff781d5a6f0499510ec86175b14227915376
Jonghun Park <jonghun.criss.android@gmail.com>
2020-09-15 16:30:00 +0900

    add startup project

---
+++ startup/src/test/java/com/simplify/startup/StartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplify.startup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
